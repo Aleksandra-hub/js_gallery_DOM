78421071a0d8cf36da1096eae3f4636f258cff52
Oleksandra Kuziura <radioday2014@gmail.com>
2024-12-22 16:44:34 +0200

    make build

---
+++ index.06367edd.js
@@ -0,0 +1,2 @@
+document.querySelector("#thumbs").onclick=function(e){var r=e.target,t=document.querySelector("#largeImg");if("IMG"===r.tagName)return t.src=r.src,!1};
+//# sourceMappingURL=index.06367edd.js.map

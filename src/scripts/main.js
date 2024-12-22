e4d25b400f999c1e3fc8a2228a98b8d426114fd7
Oleksandra Kuziura <radioday2014@gmail.com>
2024-12-22 16:35:33 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const list = document.querySelector('#thumbs');
+
+list.onclick = function (item) {
+  const target = item.target;
+  const largeImg = document.querySelector('#largeImg');
+
+  largeImg.src = target.src;
+
+  return false;
+};
